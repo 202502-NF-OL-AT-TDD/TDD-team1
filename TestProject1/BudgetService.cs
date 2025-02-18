a0b4cc2989cc0103bbf2d38641aa4fea92bef22e
joeychen <joeychen@odd-e.com>
2025-02-18 22:27:27 +0800

    make method non-static: OverlappingDays(), move method to Period class

--- TestProject1/BudgetService.cs
+++ TestProject1/BudgetService.cs
@@ -21,7 +21,7 @@ public class BudgetService
 
         foreach (var budget in budgets)
         {
-            var effectiveDays = OverlappingDays(new Period(start, end), budget);
+            var effectiveDays = new Period(start, end).OverlappingDays(budget);
 
             var daysInMonth = DateTime.DaysInMonth(budget.FirstDay().Year, budget.FirstDay().Month);
 
@@ -29,18 +29,5 @@ public class BudgetService
         }
 
         return totalAmount;
-    }
-
-    private static int OverlappingDays(Period period, Budget budget)
-    {
-        if (period.End < budget.FirstDay() || period.Start > budget.LastDay())
-        {
-            return 0;
-        }
-
-        var effectiveStart = period.Start > budget.FirstDay() ? period.Start : budget.FirstDay();
-        var effectiveEnd = period.End < budget.LastDay() ? period.End : budget.LastDay();
-
-        return (effectiveEnd - effectiveStart).Days + 1;
     }
 }
--- TestProject1/Period.cs
+++ TestProject1/Period.cs
@@ -11,4 +11,17 @@ public class Period
     public DateTime End { get; private set; }
 
     public DateTime Start { get; private set; }
+
+    public int OverlappingDays(Budget budget)
+    {
+        if (End < budget.FirstDay() || Start > budget.LastDay())
+        {
+            return 0;
+        }
+
+        var effectiveStart = Start > budget.FirstDay() ? Start : budget.FirstDay();
+        var effectiveEnd = End < budget.LastDay() ? End : budget.LastDay();
+
+        return (effectiveEnd - effectiveStart).Days + 1;
+    }
 }
