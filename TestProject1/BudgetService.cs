444464043440fa81057fc8fa5cdf9437cd7fdfe9
joeychen <joeychen@odd-e.com>
2025-02-18 22:26:51 +0800

    move Period class to independent file

--- TestProject1/BudgetService.cs
+++ TestProject1/BudgetService.cs
@@ -1,18 +1,5 @@
 namespace ConsoleApp1;
 
-public class Period
-{
-    public Period(DateTime start, DateTime end)
-    {
-        Start = start;
-        End = end;
-    }
-
-    public DateTime End { get; private set; }
-
-    public DateTime Start { get; private set; }
-}
-
 public class BudgetService
 {
     private readonly IBudgetRepo _budgetRepo;
